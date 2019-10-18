d26315a260e48171ff44f8420ed1a66843d6dee2
J. Famous <joshua.famous@gmail.com>
2019-10-17 23:43:16 -0400

    Perfect scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-function scrollTo(id){
+function scrollSection(id){
 
-	document.getElementById(id).scrollIntoView({behavior:'smooth'});
+	var offset = -(document.getElementById('navbar').offsetHeight) + 2;
+	var element = document.getElementById(id);
+	var yscroll = element.getBoundingClientRect().top + window.pageYOffset + offset;
+	window.scrollTo({top:yscroll, behavior:'smooth'});
 
 }
