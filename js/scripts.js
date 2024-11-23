b26bff6943d4775886a78749471d23516a8d2b63
Joshua Famous <joshua.famous@gmail.com>
2024-11-22 20:51:40 -0500

    Nov 2024 Updates

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,14 @@ function scrollSection(id){
 	window.scrollTo({top:yscroll, behavior:'smooth'});
 
 }
+
+function toggleSkillSection(sectionName){
+	var fs = document.getElementById(sectionName + "-text").style.fontSize;
+	if(fs != "1rem"){
+		document.getElementById(sectionName + "-text").style.fontSize = "1rem";
+		document.getElementById(sectionName).innerHTML = "Show Less";
+	} else {
+		document.getElementById(sectionName + "-text").style.fontSize = 0;
+		document.getElementById(sectionName).innerHTML = "Show More";
+	}
+}
